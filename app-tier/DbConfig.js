f2c91b59dbb41db1c4ccacb6dccf7752a5d075ac
Stark <karneatharv1920@gmail.com>
2024-01-11 20:45:02 +0530

    first commit

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.ch4kqym0ah95.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Stark20020',
+    DB_DATABASE : 'webappdb'
+});
